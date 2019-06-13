65f78b160d51317be5a0cd251c2a8a8419a35100
Web Dev <webdev@Webs-MBP.lan>
2019-06-13 16:23:18 +0100

    welcome screen, will be replaced later

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -4,7 +4,7 @@
         <meta charset="utf-8">
         <meta name="viewport" content="width=device-width, initial-scale=1">
 
-        <title>Laravel</title>
+        <title>Express Merchants</title>
 
         <!-- Fonts -->
         <link href="https://fonts.googleapis.com/css?family=Nunito:200,600" rel="stylesheet">
@@ -81,7 +81,7 @@
 
             <div class="content">
                 <div class="title m-b-md">
-                    Laravel
+                    Express Merchants
                 </div>
 
                 <div class="links">
