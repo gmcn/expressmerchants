013e0614f01e96ef40f1f6ef4e306e49fc4aac07
Web Dev <webdev@Webs-MBP.lan>
2019-06-26 14:38:40 +0100

    added disable/enable feature to user management

--- app/Http/Controllers/UserController.php
+++ app/Http/Controllers/UserController.php
@@ -8,6 +8,7 @@ use App\User;
 use Redirect;
 use Auth;
 use DB;
+use App\Po;
 
 class UserController extends Controller
 {
@@ -17,12 +18,12 @@ class UserController extends Controller
     if (Auth::user()->accessLevel == '1') {
       // $users = User::all();
       $users = DB::table('users')->paginate(25);
+
     } elseif (Auth::user()->accessLevel == '2') {
       $users = User::where('companyId','=', Auth::user()->companyId)->paginate(25);
     } else {
       return Redirect::to('/');
     }
-
       return view('userlist', compact('users'));
   }
 
@@ -34,5 +35,23 @@ class UserController extends Controller
       $user->delete();
 
       return Redirect::to('userlist')->with('message', 'User removed');
+  }
+
+  public function disableUser($id)
+  {
+
+      User::where('id', $id)
+      ->update(['disabled' => '1']);
+
+      return Redirect::to('userlist')->with('message', 'User disabled');
+  }
+
+  public function enableUser($id)
+  {
+
+      User::where('id', $id)
+      ->update(['disabled' => '']);
+
+      return Redirect::to('userlist')->with('message', 'User enabled');
   }
 }
