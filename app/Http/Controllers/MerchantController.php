f818e1cb3a64e4ab40891ccd302eea8a158df419
Web Dev <webdev@Webs-MBP.lan>
2019-07-05 16:29:50 +0100

    trial/error for google maps

--- app/Http/Controllers/MerchantController.php
+++ app/Http/Controllers/MerchantController.php
@@ -9,6 +9,7 @@ use Redirect;
 use Illuminate\Support\Facades\Input;
 use Auth;
 use DB;
+use Response;
 
 class MerchantController extends Controller
 {
@@ -31,6 +32,45 @@ class MerchantController extends Controller
     Merchant::create($request->toArray());
 
     return Redirect::to('merchant-list')->with('message', 'Merchant successfully added');
+
+  }
+
+  public function findMerchant()
+  {
+    // $results = Merchant::all();
+    //
+    // $xml = new \XMLWriter();
+    // $xml->openMemory();
+    // $xml->startDocument();
+    // $xml->startElement('markers');
+    // foreach($results as $result) {
+    //     $xml->startElement('marker');
+    //     $xml->writeAttribute('id', $result->id);
+    //     $xml->writeAttribute('name', $result->merchantName);
+    //     $xml->writeAttribute('address', $result->merchantAddress1);
+    //     $xml->writeAttribute('lat', $result->lat);
+    //     $xml->writeAttribute('lng', $result->lng);
+    //     $xml->writeAttribute('distance', $result->distance);
+    //     $xml->endElement();
+    // }
+    // $xml->endElement();
+    // $xml->endDocument();
+    //
+    // $content = $xml->outputMemory();
+    // $xml = null;
+    //
+    // return response($content)->header('Content-Type', 'text/xml');
+
+
+    return view('merchant-find', compact('results', 'content'));
+
+
+  }
+
+  public function resultsMerchant()
+  {
+
+    return view('merchant-find');
 
   }
 
