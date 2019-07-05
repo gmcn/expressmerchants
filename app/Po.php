ae42817c985c62dd1a9881de2f29481a5fe35322
Web Dev <webdev@Webs-MBP.lan>
2019-07-05 16:29:18 +0100

    new fields for PO

--- app/Po.php
+++ app/Po.php
@@ -7,6 +7,6 @@ use Illuminate\Database\Eloquent\Model;
 class Po extends Model
 {
     protected $fillable = [
-        'u_id', 'companyId', 'poType', 'poPurpose', 'poProject', 'poProjectLocation', 'poInvoice', 'poPod', 'poCompanyPo', 'poCancelled', 'poCompleted', 'updated_at'
+        'u_id', 'companyId', 'selectMerchant', 'inputMerchant', 'poType', 'poPurpose', 'poProject', 'poProjectLocation', 'poInvoice', 'poPod', 'poCompanyPo', 'poCancelled', 'poCompleted', 'updated_at'
     ];
 }
