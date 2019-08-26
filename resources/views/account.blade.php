c46ca064386ff1c83b9ae01b917978ba2f10780c
Web Dev <webdev@webs-mbp.home>
2019-08-27 00:01:35 +0100

    updated design for logged in user account details

--- resources/views/account.blade.php
+++ resources/views/account.blade.php
@@ -1,27 +1,34 @@
 @extends('layouts.app')
 
 @section('content')
-<?php $title = 'User Account <span>Details</span>'; ?>
-<div class="container-fluid po_list">
-    <div class="row justify-content-center">
-      <div class="col-md-5 col-lg-3 contactdetails">
+<?php $title = 'My Account <span>Details</span>'; ?>
 
-          <h3>Company</h3>
-          <p class="company">{{ $company->companyName }}</p>
-          <h3>Operator</h3>
-          <p>{{ Auth::user()->name }}</p>
+
+<div class="container-fluid account">
+    <div class="row">
+      <div class="col-md-3 account_details matchheight">
+
+        <div class="label">
+          Details
+        </div>
+
+        <p><img src="{{ asset('/images/account-details.svg') }}" alt="User Account Details"> {{ Auth::user()->name }}</p>
+
+        <p><img src="{{ asset('/images/phone_icon.svg') }}" alt="User Account Details"> {{ Auth::user()->phone }}</p>
+
+        <p><img src="{{ asset('/images/email-icon.svg') }}" alt="User Account Details"> <a href="mailto:{{ Auth::user()->email }}">{{ Auth::user()->email }}</a></p>
+
+        <p><img src="{{ asset('/images/company-icon.svg') }}" alt="User Account Details"> {{ $company->companyName }}</p>
 
       </div>
-        <div class="col-md-7 col-lg-9">
-
-          <p>Name <br /> {{ Auth::user()->name }}</p>
-
-          <p>Phone <br /> {{ Auth::user()->phone }}</p>
-
-          <p>Email <br /> <a href="mailto:{{ Auth::user()->email }}">{{ Auth::user()->email }}</a></p>
-
-
-      </div>
+        <div class="col-md-9 account_quote d-none d-md-block matchheight">
+          <!-- <div class="blue"> -->
+              <h2><span>the right products <br />
+              the first time</span> <br />
+              one invoice.</h2>
+              <img src="{{ asset('/images/em_logomark.svg') }}" alt="">
+          <!-- </div> -->
+        </div>
     </div>
 </div>
 @endsection
