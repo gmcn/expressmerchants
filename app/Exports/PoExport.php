731ae89407c88ba341ba989efda1c8f60953ab65
Web Dev <webdev@Webs-MBP.lan>
2019-10-14 11:38:27 +0100

    allowing flexibility to PO csv export

--- app/Exports/PoExport.php
+++ app/Exports/PoExport.php
@@ -4,6 +4,9 @@ namespace App\Exports;
 
 use App\Po;
 use Maatwebsite\Excel\Concerns\FromCollection;
+use Auth;
+
+use Illuminate\Http\Request;
 
 class PoExport implements FromCollection
 {
@@ -12,6 +15,22 @@ class PoExport implements FromCollection
     */
     public function collection()
     {
-        return Po::all();
+
+        $u_id = \Request::get('u_id');
+        $poPod = \Request::get('poPod');
+        $company_id = \Request::get('company_id');
+        $date = \Request::get('date');
+
+        if (Auth::user()->accessLevel == '1') {
+          return Po::all();
+        }
+
+        if (Auth::user()->accessLevel == '2') {
+          return Po::where('companyId', Auth::user()->companyId)->get();
+        }
+
+        if (Auth::user()->accessLevel == '3') {
+          return Po::where('u_id', '=', Auth::user()->id)->where('created_at', 'LIKE', "%$date%")->get();
+        }
     }
 }
